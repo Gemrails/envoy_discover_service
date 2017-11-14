6024bf27f19269ed67df180aa6f398c0bfe3f7f0
崔斌 <cuiminmm@gmail.com>
2017-11-14 18:32:14 +0800

    [ADD]envoy dockerfile，config，start script

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+From lyft/envoy-alpine
+MAINTAINER pujielan@gmail.com
+
+#RUN apk --no-cache add curl vim
+ADD start.sh /root/start.sh
+RUN chmod 755 /root/start.sh
+ENV ENVOY_BINARY="/usr/local/bin/envoy"
+
+WORKDIR /root
+
+CMD ["./start.sh"]
+
+
+
---
+++ start.sh
@@ -0,0 +1,136 @@
+#!/bin/sh
+set -e
+
+func_init(){
+    func_check_env
+}
+
+func_set_service_nodes(){
+    mm=`echo $DEPEND_SERVICE | sed "s/,/ /g"`
+    SERVICE_NODE=""
+    for nn in $mm 
+    do 
+        ALIAS=`echo $nn | awk -F ":" '{print $1}'`
+        if [[ $SERVICE_NODE == "" ]];then
+            SERVICE_NODE=$ALIAS
+            continue
+        fi
+        SERVICE_NODE=$SERVICE_NODE"_"$ALIAS
+    done
+    echo $SERVICE_NODE
+}
+
+func_modify_conf(){
+cat <<EOF>> $CONFIGPATH
+{
+  "listeners": [],
+  "lds": {
+    "cluster": "lds",
+    "refresh_delay_ms": 30000
+  },
+  "admin": {
+    "access_log_path": "/var/log/envoy/admin_access.log", 
+    "address": "tcp://0.0.0.0:65534"
+  },  
+  "cluster_manager": {
+    "sds": {
+      "cluster": {
+        "name": "sds", 
+        "connect_timeout_ms": 250, 
+        "type": "strict_dns", 
+        "lb_type": "round_robin", 
+        "hosts": [
+          {
+            "url": "tcp://${DISCOVERSERVER}:${DISCOVERPORT}"
+          }
+        ]
+      }, 
+      "refresh_delay_ms": 5000
+    }, 
+    "cds": {
+      "cluster": {
+        "name": "cds", 
+        "connect_timeout_ms": 250, 
+        "type": "strict_dns", 
+        "lb_type": "round_robin", 
+        "hosts": [
+          {
+            "url": "tcp://${DISCOVERSERVER}:${DISCOVERPORT}"
+          }
+        ]
+      }, 
+      "refresh_delay_ms": 30000
+    }, 
+    "clusters": [
+      {
+        "name": "lds", 
+        "connect_timeout_ms": 250, 
+        "type": "strict_dns", 
+        "lb_type": "round_robin", 
+        "hosts": [
+          {
+            "url": "tcp://${DISCOVERSERVER}:${DISCOVERPORT}"
+          }
+        ]
+      }
+    ]
+  }
+}
+EOF
+}
+
+func_update_conf(){
+    sed -i 's/DISCOVERSERVER/${DISCOVERSERVER}/g' $CONFIGPATH
+    if [[ $? != 0 ]];then
+        exit 8
+    fi
+    sed -i 's/DISCOVERPORT/$DISCOVERPORT/g' $CONFIGPATH
+    if [[ $? != 0 ]];then
+        exit 8
+    fi
+}
+
+func_main(){
+    func_init
+    func_set_service_nodes
+    #func_update_conf
+    func_modify_conf
+    $ENVOY_BINARY -c $CONFIGPATH --service-cluster $TENANT_ID --service-node $SERVICE_NODE
+}
+
+func_check_env(){
+    #@ DISCOVERSERVER DISCOVERPORT TENANT_ID DEPEND_SERVICE
+    if [[ -z $DISCOVERSERVER ]];then
+        DISCOVERSERVER="127.0.0.1" 
+        echo "env DISCOVERSERVER set to default localhost."
+    fi
+    if [[ -z $DISCOVERPORT ]];then
+        DISCOVERPORT=6100 
+        echo "env DISCOVERPORT set to default 6100"
+    fi
+    if [[ -z $CONFIGPATH ]];then
+       CONFIGPATH="/root/envoy_config.json"
+    fi
+    if [[ -z $ENVOY_BINARY ]];then
+        ENVOY_BINARY="/usr/local/bin/envoy"
+    fi
+    if [[ -z $TENANT_ID ]];then
+        echo "need env TENANT_ID"
+        exit 9
+    fi
+    if [[ -z $DEPEND_SERVICE ]];then
+        echo "need env DEPEND_SERVICE"
+        exit 9
+    fi
+}
+
+func_test(){
+    DEPEND_SERVICE=gr6adef3:9a1576b6a4a8e3185646cdae916adef3,gr123123:678906781923123sdfssdfsgsgs,gr44444:adsfadfafadfasdfasdfasdf
+    TENANT_ID=1b05123123124tenantid
+}
+
+if [[ $1 == "test" ]];then
+    func_test
+fi
+
+func_main
